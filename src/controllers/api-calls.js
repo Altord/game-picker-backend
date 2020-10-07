b42343c7b5dbbd244d426be05c435137dbdbdaa8
Altord <altord@protonmail.com>
2020-10-06 22:22:00 -0400

    Fiddled with color again

--- src/controllers/api-calls.js
+++ src/controllers/api-calls.js
@@ -2,7 +2,8 @@ const config = require('../utils/config')
 const express = require('express')
 const axios = require('axios')
 const apiRouter = express.Router()
-
+const { createCanvas, loadImage } = require('canvas');
+const FastAverageColor = require('fast-average-color');
 
 const today = new Date()
 const date1 = (new Date().setDate(today.getDate()))/1000
@@ -11,6 +12,39 @@ const date14 = (new Date().setDate(today.getDate()-14))/1000
 const roundedDate60 = Math.round(date60)
 const roundedDate14 = Math.round(date14)
 const roundedDate1 = Math.round(date1)
+
+const fac = new FastAverageColor();
+
+//General insert function
+
+Array.prototype.insert = function ( index, item ) {
+    this.splice( index, 0, item );
+};
+
+//color function
+async function printAverageColor(filename) {
+    const img = await loadImage(filename);
+    const { width, height } = img;
+
+    const canvas =  createCanvas(width, height);
+    const ctx =  canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+
+    const imageData =  ctx.getImageData(0, 0, width, height);
+
+    let imageColorArray= [
+        {SAC:   fac.getColorFromArray4(imageData.data,  { algorithm: 'simple'}) },
+        {SQAC:   fac.getColorFromArray4(imageData.data) },
+        {DAC:  fac.getColorFromArray4(imageData.data, { algorithm: 'dominant'})}
+    ]
+
+    // console.log(`Filename: ${filename}, size: ${width}×${height}`);
+    //console.log('// [red, green, blue, opacity]');
+    //console.log(imageColorArray)
+    return imageColorArray
+
+}
+
 
 //Basic Search function
 apiRouter.post('/api-router-search', (req,res) =>{
@@ -26,9 +60,21 @@ apiRouter.post('/api-router-search', (req,res) =>{
         },
         data: `fields id,name,platforms.*,genres.*,aggregated_rating,cover.*; search "${req.body.searchValue}"; limit 1;`
     },[])
-        .then(response => {
-            res.send(response.data)
-           // console.log(response.data)
+        .then(response =>{
+            const dataResponse = response.data
+            const colorResponse =
+                Promise.all(
+                    Array.from({ length: 7 }, (_, idx) =>
+                        printAverageColor(response.data[idx].cover.url.replace("//", "https://")
+                        )));
+
+
+            return Promise.all([dataResponse,colorResponse])
+
+
+        })
+        .then(color=>{
+            res.send(color)
         })
 
         .catch(err => {
@@ -51,10 +97,21 @@ apiRouter.post('/api-router-popularity', (req,res) =>{
         },
         data: `fields name,hypes,aggregated_rating,genres.*,cover.*,collection,platforms.*; where first_release_date > ${roundedDate60} & rating>= 70 & aggregated_rating >=80 & hypes >= 1 ; limit: 10; sort hypes desc;`
     })
-        .then(response => {
-            res.send(response.data)
-            //console.log(response.data)
+        .then(response =>{
+            const dataResponse = response.data
+            const colorResponse =
+                Promise.all(
+                    Array.from({ length: 7 }, (_, idx) =>
+                        printAverageColor(response.data[idx].cover.url.replace("//", "https://")
+                        )));
 
+
+            return Promise.all([dataResponse,colorResponse])
+
+
+        })
+        .then(color=>{
+            res.send(color)
         })
 
         .catch(err => {
@@ -79,7 +136,7 @@ apiRouter.post('/api-router-trending', (req,res) =>{
     })
         .then(response => {
             res.send(response.data)
-           // console.log(response.data)
+            // console.log(response.data)
         })
 
         .catch(err => {
@@ -99,7 +156,7 @@ apiRouter.post('/api-router-soon', (req,res) =>{
             'Authorization': `Bearer ${config.IDGB.AT}`,
 
         },
-        data: `fields game.cover.*,game.*; where date <= ${roundedDate1} & human != "TBD"; limit 16; sort date desc;`
+        data: `fields game.cover.*,game.*; where date <= ${roundedDate1} & human != "TBD"; limit 30; sort date desc;`
     })
         .then(response => {
             const uniqueNames = new Set();
@@ -133,7 +190,7 @@ apiRouter.post('/api-router-anticipated', (req,res) =>{
             'Authorization': `Bearer ${config.IDGB.AT}`,
 
         },
-        data: `fields name,hypes,aggregated_rating,genres.*,cover.*,status,release_dates.*; limit 10; where release_dates.date >= ${roundedDate14};`
+        data: `fields name,hypes,aggregated_rating,genres.*,cover.*,status,release_dates.*,follows; limit 10; sort follows desc; where (follows != null & first_release_date > ${roundedDate1} );`
     })
         .then(response => {
             res.send(response.data)
@@ -158,12 +215,30 @@ apiRouter.get('/test', (req,res) =>{
             'Authorization': `Bearer ${config.IDGB.AT}`,
 
         },
-        data: `fields name,hypes,aggregated_rating,genres.*,cover.*,status,release_dates.*; limit 10; where release_dates.date >= ${roundedDate14};`
+        data: `fields name,hypes,aggregated_rating,genres.*,cover.*,collection,platforms.*; where first_release_date > ${roundedDate60} & rating>= 70 & aggregated_rating >=80 & hypes >= 1 ; limit: 10; sort hypes desc;`
     })
-        .then(response => {
-            res.send(response.data)
-            //console.log(response.data)
+        /*.then(response=>{
+            let array=[]
+            array.push(response.data)
+            return array
+        })*/
+        .then(response =>{
+            const dataResponse = response.data
+            const colorResponse =
+                Promise.all(
+                Array.from({ length: 7 }, (_, idx) =>
+                    printAverageColor(response.data[idx].cover.url.replace("//", "https://")
+                    )));
+
+
+           return Promise.all([dataResponse,colorResponse])
+
+
         })
+        .then(color=>{
+            res.send(color)
+        })
+
 
         .catch(err => {
             console.error(err);
@@ -208,11 +283,8 @@ apiRouter.get('/test', (req,res) =>{
         return authToken.access_data})
         .catch(error => (console.log(error)) )
     console.log(authToken)
-
 });
 */
 
 
 module.exports = apiRouter
-
-
